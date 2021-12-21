17357b6101a361bf83b1c70b28ef6a5609f40240
Mariia <mariiademidova@outlook.com>
2021-12-21 15:11:33 +0300

    Добавьте файлы проекта.

---
+++ Homework_6.2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строчку без знаков препинания:");
+            string s = Console.ReadLine();
+            string sWithout = s.Replace(" ", "");
+            char[] reverseS = sWithout.ToCharArray();
+            Array.Reverse(reverseS);
+            string s2 = new string(reverseS);
+            bool equal = String.Equals(sWithout, s2, StringComparison.OrdinalIgnoreCase);
+            Console.WriteLine();
+            if (equal == true)
+            {
+                Console.WriteLine("Строка является палиндромом");
+            }
+            else
+            {
+                Console.WriteLine("Строка не является палиндромом");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework_6/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите строчку без знаков препинания:");
+            string firstString = Console.ReadLine();
+            string[] word = firstString.Split();
+            int maxL = 0;
+            Console.WriteLine();
+            Console.WriteLine("Самое длинное слово:");
+            foreach (string i in word)
+            {
+                if (i.Length > maxL)
+                {
+                    maxL = i.Length;
+                }
+            }
+            foreach (string i in word)
+            {
+                if (i.Length == maxL)
+                {
+                    Console.WriteLine("{0}", i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
